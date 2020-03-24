5710ca7e3893811e01f3af5bc27a8e4b71640168
luoChen <hank.luo@xianghuanji.com>
2020-03-24 18:30:21 +0800

    将APP添加进来用于调试

---
+++ matrix-master/matrix/matrix-android/app/src/main/java/sample/tencent/matrix/MatrixApplication.java
@@ -0,0 +1,142 @@
+/*
+ * Tencent is pleased to support the open source community by making wechat-matrix available.
+ * Copyright (C) 2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.tencent.matrix;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.tencent.matrix.Matrix;
+import com.tencent.matrix.iocanary.IOCanaryPlugin;
+import com.tencent.matrix.iocanary.config.IOConfig;
+import com.tencent.matrix.resource.ResourcePlugin;
+import com.tencent.matrix.resource.config.ResourceConfig;
+import com.tencent.matrix.trace.TracePlugin;
+import com.tencent.matrix.trace.config.TraceConfig;
+import com.tencent.matrix.util.MatrixLog;
+import com.tencent.sqlitelint.SQLiteLint;
+import com.tencent.sqlitelint.SQLiteLintPlugin;
+import com.tencent.sqlitelint.config.SQLiteLintConfig;
+
+import sample.tencent.matrix.config.DynamicConfigImplDemo;
+import sample.tencent.matrix.listener.TestPluginListener;
+import sample.tencent.matrix.sqlitelint.TestSQLiteLintActivity;
+
+/**
+ * Created by caichongyang on 17/5/18.
+ */
+
+public class MatrixApplication extends Application {
+    private static final String TAG = "Matrix.Application";
+
+    private static Context sContext;
+
+    private static SQLiteLintConfig initSQLiteLintConfig() {
+        try {
+            /**
+             * HOOK模式下，SQLiteLint会自己去获取所有已执行的sql语句及其耗时(by hooking sqlite3_profile)
+             * @see 而另一个模式：SQLiteLint.SqlExecutionCallbackMode.CUSTOM_NOTIFY , 则需要调用 {@link SQLiteLint#notifySqlExecution(String, String, int)}来通知
+             * SQLiteLint 需要分析的、已执行的sql语句及其耗时
+             * @see TestSQLiteLintActivity#doTest()
+             */
+            return new SQLiteLintConfig(SQLiteLint.SqlExecutionCallbackMode.HOOK);
+        } catch (Throwable t) {
+            return new SQLiteLintConfig(SQLiteLint.SqlExecutionCallbackMode.HOOK);
+        }
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        Log.e(TAG, "attachBaseContext");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DynamicConfigImplDemo dynamicConfig = new DynamicConfigImplDemo();
+        boolean matrixEnable = dynamicConfig.isMatrixEnable();
+        boolean fpsEnable = dynamicConfig.isFPSEnable();
+        boolean traceEnable = dynamicConfig.isTraceEnable();
+
+        sContext = this;
+        MatrixLog.i(TAG, "MatrixApplication.onCreate");
+
+        Matrix.Builder builder = new Matrix.Builder(this);
+        builder.patchListener(new TestPluginListener(this));
+
+        //trace
+        TraceConfig traceConfig = new TraceConfig.Builder()
+                .dynamicConfig(dynamicConfig)
+                .enableFPS(fpsEnable)
+                .enableEvilMethodTrace(traceEnable)
+                .enableAnrTrace(traceEnable)
+                .enableStartup(traceEnable)
+                .splashActivities("sample.tencent.matrix.SplashActivity;")
+                .isDebug(true)
+                .isDevEnv(true)
+                .build();
+
+        TracePlugin tracePlugin = (new TracePlugin(traceConfig));
+        builder.plugin(tracePlugin);
+
+        if (matrixEnable) {
+
+            //resource
+            Intent intent = new Intent();
+            ResourceConfig.DumpMode mode = ResourceConfig.DumpMode.AUTO_DUMP;
+            MatrixLog.i(TAG, "Dump Activity Leak Mode=%s", mode);
+            intent.setClassName(this.getPackageName(), "com.tencent.mm.ui.matrix.ManualDumpActivity");
+            ResourceConfig resourceConfig = new ResourceConfig.Builder()
+                    .dynamicConfig(dynamicConfig)
+                    .setAutoDumpHprofMode(mode)
+//                .setDetectDebuger(true) //matrix test code
+                    .setNotificationContentIntent(intent)
+                    .build();
+            builder.plugin(new ResourcePlugin(resourceConfig));
+            ResourcePlugin.activityLeakFixer(this);
+
+            //io
+            IOCanaryPlugin ioCanaryPlugin = new IOCanaryPlugin(new IOConfig.Builder()
+                    .dynamicConfig(dynamicConfig)
+                    .build());
+            builder.plugin(ioCanaryPlugin);
+
+
+            // prevent api 19 UnsatisfiedLinkError
+            //sqlite
+            SQLiteLintConfig sqlLiteConfig;
+            try {
+                sqlLiteConfig = new SQLiteLintConfig(SQLiteLint.SqlExecutionCallbackMode.CUSTOM_NOTIFY);
+            } catch (Throwable t) {
+                sqlLiteConfig = new SQLiteLintConfig(SQLiteLint.SqlExecutionCallbackMode.CUSTOM_NOTIFY);
+            }
+            builder.plugin(new SQLiteLintPlugin(sqlLiteConfig));
+        }
+
+        Matrix.init(builder.build());
+
+        //start only startup tracer, close other tracer.
+        tracePlugin.start();
+    }
+
+
+    public static Context getContext() {
+        return sContext;
+    }
+}
---
+++ matrix-master/matrix/matrix-android/app/src/main/java/sample/tencent/matrix/trace/TestFpsActivity.java
@@ -0,0 +1,118 @@
+/*
+ * Tencent is pleased to support the open source community by making wechat-matrix available.
+ * Copyright (C) 2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.tencent.matrix.trace;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.print.PrinterId;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.tencent.matrix.Matrix;
+import com.tencent.matrix.plugin.Plugin;
+import com.tencent.matrix.trace.TracePlugin;
+import com.tencent.matrix.trace.listeners.IDoFrameListener;
+import com.tencent.matrix.util.MatrixLog;
+
+import java.util.Random;
+import java.util.concurrent.Executor;
+
+import sample.tencent.matrix.R;
+import sample.tencent.matrix.issue.IssueFilter;
+
+/**
+ * Created by caichongyang on 2017/11/14.
+ */
+
+public class TestFpsActivity extends Activity {
+    private static final String TAG = "Matrix.TestFpsActivity";
+    private ListView mListView;
+    private static HandlerThread sHandlerThread = new HandlerThread("test");
+
+    static {
+        sHandlerThread.start();
+    }
+
+    private int count;
+    private long time = System.currentTimeMillis();
+    private IDoFrameListener mDoFrameListener = new IDoFrameListener(new Executor() {
+        Handler handler = new Handler(sHandlerThread.getLooper());
+
+        @Override
+        public void execute(Runnable command) {
+            handler.post(command);
+        }
+    }) {
+        @Override
+        public void doFrameAsync(String visibleScene, long taskCost, long frameCostMs, int droppedFrames, boolean isContainsFrame) {
+            super.doFrameAsync(visibleScene, taskCost, frameCostMs, droppedFrames, isContainsFrame);
+            count += droppedFrames;
+            MatrixLog.i(TAG, "[doFrameSync] scene:" + visibleScene + " droppedFrames:" + droppedFrames);
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.test_fps_layout);
+
+        IssueFilter.setCurrentFilter(IssueFilter.ISSUE_TRACE);
+
+        Matrix.with().getPluginByClass(TracePlugin.class).getFrameTracer().onStartTrace();
+        Matrix.with().getPluginByClass(TracePlugin.class).getFrameTracer().addListener(mDoFrameListener);
+
+        time = System.currentTimeMillis();
+        mListView = (ListView) findViewById(R.id.list_view);
+        String[] data = new String[200];
+        for (int i = 0; i < 200; i++) {
+            data[i] = "MatrixTrace:" + i;
+        }
+        mListView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, data) {
+            Random random = new Random();
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                int rand = random.nextInt(10);
+                if (rand % 3 == 0) {
+                    try {
+                        Thread.sleep(25);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return super.getView(position, convertView, parent);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MatrixLog.i(TAG, "[onDestroy] count:" + count + " time:" + (System.currentTimeMillis() - time) + "");
+        Matrix.with().getPluginByClass(TracePlugin.class).getFrameTracer().removeListener(mDoFrameListener);
+        Matrix.with().getPluginByClass(TracePlugin.class).getFrameTracer().onCloseTrace();
+    }
+}
